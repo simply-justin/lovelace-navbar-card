2093cf52bd8e02fee6e8b1d8528fa15be992938d
Jose Luis Álvarez <joseluisalvquiroga@gmail.com>
2024-10-28 13:36:58 +0100

    Feature/styles refactor (#2)
    * Add optional desktop_min_width parameter to change style of navbar card
    
    * Change layout based on edit mode

--- src/navbar-card.ts
+++ src/navbar-card.ts
@@ -1,6 +1,7 @@
 import { HomeAssistant } from 'custom-card-helpers';
 import { css, CSSResultGroup, html, LitElement } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
+import { version } from '../package.json';
 
 // TODO add proper typing to window
 // @ts-ignore
@@ -20,6 +21,7 @@ type NavbarCardConfig = {
     icon: string;
     icon_selected?: string;
   }[];
+  desktop_min_width?: number;
 };
 
 @customElement('navbar-card')
@@ -28,40 +30,84 @@ export class NavbarCard extends LitElement {
   private _config?: NavbarCardConfig;
 
   @state()
-  private hass?: HomeAssistant;
+  private screenWidth?: number;
+
+  @state()
+  private inEditMode?: boolean;
+
+  constructor() {
+    super();
+    console.info(
+      `%c navbar-card %c ${version} `,
+      // Card name styles
+      'background-color: #555;\
+      padding: 6px 4px;\
+      color: #fff;\
+      border-radius: 10px 0 0 10px;',
+      // Card version styles
+      'background-color: #00abd1; \
+      color: #fff;\
+      padding: 6px 4px;\
+      border-radius: 0 10px 10px 0;',
+    );
+  }
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    // Initialize screen size listener
+    window.addEventListener('resize', this._onResize);
+    this.screenWidth = window.innerWidth;
+
+    // Check if dashboard is in edit mode
+    this.inEditMode =
+      this.parentElement?.closest('hui-card-edit-mode') !== null;
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    // Remove event listeners
+    window.removeEventListener('resize', this._onResize);
+  }
+
+  private _onResize = () => {
+    this.screenWidth = window.innerWidth;
+  };
 
   setConfig(config) {
     this._config = config;
   }
 
   protected render() {
-    if (!this.hass || !this._config) {
+    if (!this._config) {
       return html``;
     }
 
-    const { routes } = this._config;
+    const isDesktopMode =
+      (this.screenWidth ?? 0) >= (this._config.desktop_min_width ?? 768);
 
-    // TODO this check does not update instantly when changing to edit mode in HA
-    const inEditMode =
-      this.parentElement?.closest('hui-card-edit-mode') !== null;
+    const { routes } = this._config;
 
     // TODO use HA ripple effect for icon button
     return html`
-      <ha-card class="container ${inEditMode ? 'relative' : 'absolute'}">
-        <div class="flex-container">
-          ${routes?.map((route, index) => {
-            const isActive = window.location.pathname == route.url;
+      <ha-card
+        class="navbar ${this.inEditMode ? 'edit-mode' : ''} ${isDesktopMode
+          ? 'desktop'
+          : 'mobile'}">
+        ${routes?.map((route, index) => {
+          const isActive = window.location.pathname == route.url;
 
-            return html`
-              <a
-                key="navbar_item_${index}"
-                class="icon-button ${isActive ? 'active' : ''}"
-                href="${route.url}">
-                <ha-icon icon="${isActive && route.icon_selected ? route.icon_selected : route.icon}"></ha-icon>
-              </div>
-            `;
-          })}
-        </div>
+          return html`
+            <a
+              key="navbar_item_${index}"
+              class="icon-button ${isActive ? 'active' : ''}"
+              href="${route.url}">
+              <ha-icon
+                icon="${isActive && route.icon_selected
+                  ? route.icon_selected
+                  : route.icon}"></ha-icon>
+            </a>
+          `;
+        })}
       </ha-card>
     `;
   }
@@ -69,30 +115,24 @@ export class NavbarCard extends LitElement {
   static get styles(): CSSResultGroup {
     // Mobile-first css styling
     return css`
-      .container {
+      .navbar {
         background: var(--card-background-color);
         border-radius: 0px;
+        /* TODO harcoded box shadow? */
         box-shadow: 0px -1px 4px 0px rgba(0, 0, 0, 0.14) !important;
-      }
-      .container.absolute {
-        width: 100vw;
-        position: fixed;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        top: unset;
-        z-index: 2;
-      }
-      .container.relative {
-        position: relative;
-      }
-      .flex-container {
         display: flex;
         flex-direction: row;
         align-items: center;
         justify-content: space-between;
         padding: 12px;
         gap: 10px;
+        width: 100vw;
+        position: fixed;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        top: unset;
+        z-index: 2; /* TODO check if needed */
       }
       .icon-button {
         text-decoration: none;
@@ -109,28 +149,31 @@ export class NavbarCard extends LitElement {
         --icon-primary-color: var(--primary-color);
       }
 
-      @media (min-width: 768px) {
-        .container {
-          border-radius: var(--ha-card-border-radius, 12px);
-          box-shadow: var(--material-shadow-elevation-2dp) !important;
-        }
-        .container.absolute {
-          width: auto;
-          left: calc(var(--mdc-drawer-width, 0px) + 16px);
-          right: unset;
-          bottom: unset;
-          top: 50%;
-          transform: translate(0, -50%);
-        }
-        .flex-container {
-          justify-content: space-evenly;
-          flex-direction: column;
-        }
-        .icon-button {
-          flex: unset;
-          width: 60px;
-          height: 60px;
-        }
+      /* Edit mode styles */
+      .navbar.edit-mode {
+        position: relative !important;
+        flex-direction: row !important;
+        left: unset !important;
+        width: auto !important;
+      }
+
+      /* Desktop mode styles */
+      .desktop.navbar {
+        border-radius: var(--ha-card-border-radius, 12px);
+        box-shadow: var(--material-shadow-elevation-2dp) !important;
+        width: auto;
+        left: calc(var(--mdc-drawer-width, 0px) + 16px);
+        right: unset;
+        bottom: unset;
+        top: 50%;
+        transform: translate(0, -50%);
+        justify-content: space-evenly;
+        flex-direction: column;
+      }
+      .desktop .icon-button {
+        flex: unset;
+        width: 60px;
+        height: 60px;
       }
     `;
   }
